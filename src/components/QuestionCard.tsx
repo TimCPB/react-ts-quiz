96bb6237424fcccc573328f24ef2ac334eff2ea5
Tim <timcpb@hotmail.com>
2021-01-16 19:05:30 +0000

    add multiple props to QuestionCard component

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,27 @@
 import React from "react";
 
-const QuestionCard = () => <div>Question Card</div>;
+type Props = {
+  question: string;
+  answer: string[];
+  callback: any;
+  userAnswer: string;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answer,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNumber} / {totalQuestions}
+    </p>
+  </div>
+);
 
 export default QuestionCard;
