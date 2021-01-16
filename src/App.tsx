ed5f8bf4028a506c3bf5db6b1be804fce0e397c4
Tim <timcpb@hotmail.com>
2021-01-16 17:58:32 +0000

    create skeleton app component w/ skeleton child components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+// components
+import QuestionCard from "./components/QuestionCard";
 
-function App() {
-  return <div className="App">Quiz</div>;
-}
+const App = () => {
+  const startTrivia = async () => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const nextQuestion = () => {};
+  return (
+    <div className="App">
+      <h1>React Quiz</h1>
+      <button classname="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score</p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="Next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+};
 
 export default App;
