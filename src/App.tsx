557dfacc9fc986dae17d6a0de845b1b80ddffb0f
Tim <timcpb@hotmail.com>
2021-01-21 16:27:36 +0000

    create shuffleArray function in utils.ts to randomise answers array

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { fetchQuizQuestions } from "./API";
 // components
 import QuestionCard from "./components/QuestionCard";
+//types
+import { Difficulty } from "./API";
 
 const TOTAL_QUESTIONS = 10;
 
@@ -12,25 +15,27 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [gameover, setGameOver] = useState(true);
 
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
   const startTrivia = async () => {};
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
   const nextQuestion = () => {};
   return (
     <div className="App">
       <h1>React Quiz</h1>
-      <button classname="start" onClick={startTrivia}>
+      <button className="start" onClick={startTrivia}>
         Start
       </button>
       <p className="score">Score</p>
       <p>Loading Questions...</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNumber={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answer}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="Next" onClick={nextQuestion}>
         Next Question
       </button>
