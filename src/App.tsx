4acff1117508babf35298b61ee3b47808a9ed04e
Tim <timcpb@hotmail.com>
2021-01-22 18:15:24 +0000

    Add correct answers to answer array in API.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { fetchQuizQuestions } from "./API";
 // components
 import QuestionCard from "./components/QuestionCard";
 //types
-import { Difficulty } from "./API";
+import { QuestionState, Difficulty } from "./API";
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
