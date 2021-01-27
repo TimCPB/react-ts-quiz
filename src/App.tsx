7068060864258dc5a3f2b26f3c4a70497966d267
Tim <timcpb@hotmail.com>
2021-01-27 11:52:31 +0000

    add styling for start and next buttons in App.styles.ts

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import QuestionCard from "./components/QuestionCard";
 //types
 import { QuestionState, Difficulty } from "./API";
 // Styles
-import { GlobalStyle } from "./App.styles";
+import { GlobalStyle, Wrapper } from "./App.styles";
 
 export type AnswerObject = {
   question: string;
@@ -72,7 +72,7 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-      <div className="App">
+      <Wrapper>
         <h1>React Quiz</h1>
         {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
           <button className="start" onClick={startTrivia}>
@@ -95,11 +95,11 @@ const App = () => {
         !loading &&
         userAnswers.length === number + 1 &&
         number !== TOTAL_QUESTIONS - 1 ? (
-          <button className="Next" onClick={nextQuestion}>
+          <button className="next" onClick={nextQuestion}>
             Next Question
           </button>
         ) : null}
-      </div>
+      </Wrapper>
     </>
   );
 };
