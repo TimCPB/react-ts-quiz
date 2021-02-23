91b1860737d41d05112045389dfa1842479f7662
Tim <timcpb@hotmail.com>
2021-02-23 16:57:34 +0000

    console.log questions

--- src/App.tsx
+++ src/App.tsx
@@ -38,9 +38,8 @@ const App = () => {
     setUserAnswers([]);
     setNumber(0);
     setLoading(false);
-
-    console.log(questions);
   };
+  console.log(questions);
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
     if (!gameOver) {
       // users answer
